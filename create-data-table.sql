a806224cf6b3d7627b5ea8118dc6a1d738adf3a6
Vicky Banks <victoria.banks052@gmail.com>
2020-08-25 11:04:26 +0100

    Created table of user data and summary table

--- create-data-table.sql
+++ create-data-table.sql
@@ -1,6 +1,44 @@
 SELECT * FROM radio1_sandbox.audio_content_enriched LIMIT 10;
 
--- 1. Get the time of playing for each piece of content for each user
+
+-- 1. Get VMB summary. This table is quicker than using 'with table AS ()'
+DROP TABLE IF EXISTS vb_vmb_summary;
+CREATE TABLE vb_vmb_summary AS
+SELECT DISTINCT master_brand_id, version_id
+FROM prez.scv_vmb;
+
+-- 2. Get all the listening per users, sum the playback time per episode to ensure 3s of listening later
+DROP TABLE IF EXISTS vb_users_listening;
+CREATE TABLE vb_users_listening AS
+SELECT DISTINCT dt :: date,
+                TRUNC(DATE_TRUNC('week', getdate() - 7)) AS week_commencing,
+                audience_id,
+                geo_country_site_visited                 as country,
+                CASE
+                    WHEN is_signed_in = TRUE AND is_personalisation_on = TRUE THEN 'signed in'
+                    ELSE 'signed out' END                AS signed_in_status,
+                version_id,
+                CASE
+                    WHEN version_id SIMILAR TO '%[0-9]%' THEN 'version_id'
+                    WHEN version_id ISNULL then NULL
+                    ELSE 'master_brand_id' END           AS id_type, -- to hep with the join
+                app_type,
+                app_name,
+                broadcast_type,
+                play_id,
+                sum(playback_time_total)                 as playback_time_total
+FROM s3_audience.audience_activity_daily_summary a
+WHERE destination = 'PS_SOUNDS'
+  AND dt BETWEEN TO_CHAR(TRUNC(DATE_TRUNC('week', getdate() - 7)), 'yyyymmdd') -- limits to the past week (Mon-Sun)
+    AND TO_CHAR(TRUNC(DATE_TRUNC('week', getdate() - 7) + 6), 'yyyymmdd')
+  --AND dt = 20200817
+  AND geo_country_site_visited IN ('United Kingdom', 'Jersey', 'Isle of Man', 'Guernsey')
+GROUP BY 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11
+;
+
+SELECT count(*) FROM vb_users_listening;
+
+-- 3. Get the time of playing for each piece of content for each user and the number of times they played it
 DROP TABLE IF EXISTS radio1_sandbox.vb_ace_scv_international_data;
 CREATE TABLE radio1_sandbox.vb_ace_scv_international_data
 (
@@ -10,8 +48,8 @@ CREATE TABLE radio1_sandbox.vb_ace_scv_international_data
     country                  varchar(255),
     signed_in_status         varchar(20),
     age_range                varchar(40),
-    app_name                 VARCHAR(255),
     app_type                 VARCHAR(255),
+    app_name                 VARCHAR(255),
     version_id               varchar(255),
     master_brand_id          varchar(255),
     broadcast_type           VARCHAR(255),
@@ -19,70 +57,120 @@ CREATE TABLE radio1_sandbox.vb_ace_scv_international_data
     stream_playing_time      INT
 ) SORTKEY (dt);
 
--- Get VMB summary. This temp table is quicker than using 'with tablee AS ()'
-CREATE TEMP TABLE vmb_summary AS
-SELECT DISTINCT master_brand_id, version_id
-FROM prez.scv_vmb
-WHERE media_type = 'audio';
-
 INSERT INTO radio1_sandbox.vb_ace_scv_international_data
-with
-    -- get the information about the user and what they watched
-    play_ids as (
-        SELECT dt :: date,
-               TRUNC(DATE_TRUNC('week', getdate() - 7)) AS week_commencing,
-               audience_id,
-               geo_country_site_visited                 as country,
-               CASE
-                   WHEN is_signed_in = TRUE AND is_personalisation_on = TRUE THEN 'signed in'
-                   ELSE 'signed out' END                AS signed_in_status,
-               version_id,
-               app_type,
-               app_name,
-               broadcast_type,
-               play_id,
-               playback_time_total
-        FROM s3_audience.audience_activity_daily_summary a
-        WHERE destination = 'PS_SOUNDS'
-          AND dt BETWEEN TO_CHAR(TRUNC(DATE_TRUNC('week', getdate() - 7)), 'yyyymmdd') -- limits to the past week (Mon-Sun)
-            AND TO_CHAR(TRUNC(DATE_TRUNC('week', getdate() - 7) + 6), 'yyyymmdd')
-          AND geo_country_site_visited IN ('United Kingdom', 'Jersey', 'Isle of Man', 'Guernsey')
-    ),
-
-    -- Add in the user's metadata
-    user_age AS (
-        SELECT DISTINCT bbc_hid3, age_range
-        FROM prez.profile_extension
-    )
 SELECT dt,
        week_commencing,
        audience_id,
        country,
        signed_in_status,
-       c.age_range,
+       CASE
+           WHEN c.age_range = 'under 10' OR c.age_range = '11-15' THEN 'Under 16'
+           WHEN c.age_range = '16-24' OR c.age_range = '25-34' THEN '16-34'
+           WHEN c.age_range = 'unknown' THEN 'unknown'
+           ELSE 'Over 35' END                           as age_range,
        app_type,
        app_name,
        a.version_id,
-       b.master_brand_id,
+       ISNULL(b.master_brand_id, a.version_id) AS master_brand_id,-- sometimes the version id IS the masterbrand. In those cases this will be NULL from the join
        broadcast_type,          --Live or OD (clip)
-       count(distinct play_id), -- counts the number of plays
+       count(distinct play_id), -- counts the number of plays per person of a piece of content
        sum(playback_time_total) as playback_time_agg
-FROM play_ids a
-         LEFT JOIN vmb_summary b
-                   ON a.version_id = b.version_id OR a.version_id = b.master_brand_id -- Join with the vmb to get the master brand -- this OR is because sometimes only the master brand is sent in the version_id column not the actual ID
-         LEFT JOIN user_age c on a.audience_id = c.bbc_hid3
+FROM vb_users_listening  a
+         LEFT JOIN vb_vmb_summary b ON a.version_id = b.version_id -- Join with the vmb to get the master brand
+         LEFT JOIN prez.profile_extension c on a.audience_id = c.bbc_hid3
+WHERE playback_time_total > 3
 GROUP BY 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11
-HAVING playback_time_agg >= 3
 ;
 
-SELECT * FROM radio1_sandbox.vb_ace_scv_international_data LIMIT 10;
+
+-- 4. Create summary table for data grouping
+DROP TABLE IF EXISTS radio1_sandbox.vb_sounds_dashboard_5_listening_international;
+CREATE TABLE radio1_sandbox.vb_sounds_dashboard_5_listening_international
+(
+    week_commencing     date DISTKEY,
+    country             varchar(255),
+    signed_in_status    varchar(255),
+    age_range           varchar(255),
+    app_type            VARCHAR(255),
+    live_aod_split      varchar(255),
+    master_brand_id     varchar(255),
+    stream_starts       BIGINT,
+    stream_playing_time BIGINT
+)SORTKEY (week_commencing );
+;
+
+-- 5. Insert in data, differentiating between signed in and signed out
+INSERT INTO radio1_sandbox.vb_sounds_dashboard_5_listening_international
+SELECT week_commencing,
+       country,
+       signed_in_status,
+       age_range,
+       app_type,
+       CASE
+           WHEN broadcast_type = 'Clip' THEN 'on-demand'
+           WHEN broadcast_type = 'Live' then 'live' END as live_aod_split,
+       master_brand_id,
+       count(stream_starts_min_3_secs)                  as stream_starts,
+       sum(stream_playing_time)                         AS stream_playing_time
+FROM radio1_sandbox.vb_ace_scv_international_data
+GROUP BY 1, 2, 3, 4, 5, 6, 7
+;
+
+-- Add in all users with no signed in status split
+INSERT INTO radio1_sandbox.vb_sounds_dashboard_5_listening_international
+SELECT week_commencing,
+       country,
+       CAST ('all' as varchar) AS signed_in_status,
+       age_range,
+       app_type,
+       CASE
+           WHEN broadcast_type = 'Clip' THEN 'on-demand'
+           WHEN broadcast_type = 'Live' then 'live' END as live_aod_split,
+       master_brand_id,
+       count(stream_starts_min_3_secs)                  as stream_starts,
+       sum(stream_playing_time)                         AS stream_playing_time
+FROM radio1_sandbox.vb_ace_scv_international_data
+GROUP BY 1,2,3,4,5,6,7
+;
+
+-- Add in all users with no app_type split
+INSERT INTO radio1_sandbox.vb_sounds_dashboard_5_listening_international
+SELECT week_commencing,
+       country,
+       signed_in_status,
+       age_range,
+       CAST('all' as varchar) AS app_type,
+       CASE
+           WHEN broadcast_type = 'Clip' THEN 'on-demand'
+           WHEN broadcast_type = 'Live' then 'live' END as live_aod_split,
+       master_brand_id,
+       count(stream_starts_min_3_secs)                  as stream_starts,
+       sum(stream_playing_time)                         AS stream_playing_time
+FROM radio1_sandbox.vb_ace_scv_international_data
+GROUP BY 1,2,3,4,5,6,7
+;
+
+--Add in all users with no signed in status split and no app type split
+INSERT INTO radio1_sandbox.vb_sounds_dashboard_5_listening_international
+SELECT week_commencing,
+       country,
+       CAST ('all' as varchar) AS signed_in_status,
+       age_range,
+       CAST('all' as varchar) AS app_type,
+       CASE
+           WHEN broadcast_type = 'Clip' THEN 'on-demand'
+           WHEN broadcast_type = 'Live' then 'live' END as live_aod_split,
+       master_brand_id,
+       count(stream_starts_min_3_secs)                  as stream_starts,
+       sum(stream_playing_time)                         AS stream_playing_time
+FROM radio1_sandbox.vb_ace_scv_international_data
+GROUP BY 1,2,3,4,5,6,7
+;
 
 
+SELECT * FROM radio1_sandbox.vb_sounds_dashboard_5_listening_international LIMIT 5;
 SELECT * FROM radio1_sandbox.audio_content_enriched  LIMIT 10;
-SELECT * FROM  radio1_sandbox.sounds_dashboard_5_listening  LIMIT 10;
 
-
-SELECT geo_country_site_visited, count(DISTINCT dt||visit_id||audience_id) AS dist_visit_id, sum(playback_time_total) AS playback_time_agg
-FROM s3_audience.audience_activity_daily_summary
-WHERE dt = 20200810 AND destination = 'PS_SOUNDS'
-GROUP BY 1;
+-- Drop tables that are no longer needed
+DROP TABLE IF EXISTS vb_users_listening;
+DROP TABLE IF EXISTS vb_vmb_summary;
