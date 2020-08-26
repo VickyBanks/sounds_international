d7c02304623b891c8fe5e83211c663b3bc1db054
Vicky Banks <victoria.banks052@gmail.com>
2020-08-26 17:42:40 +0100

    Added a missing split

--- 2_get_KPI_data.sql
+++ 2_get_KPI_data.sql
@@ -1,8 +1,10 @@
 /*
  Get the summary for the KPI dash tab
  Need the number of visitors, time spent and num stream starts
+
+ Uses this table SELECT * FROM radio1_sandbox.vb_sounds_int_users_listening LIMIT 5;
  */
-SELECT * FROM radio1_sandbox.vb_sounds_int_users_listening LIMIT 5;
+
 --1. Get the number of visitors and the length of playing time
 -- Any stream with playing time <3s is not counted so here is set to 0
 DROP TABLE IF EXISTS radio1_sandbox.vb_sounds_int_KPI_temp;
@@ -19,7 +21,8 @@ SELECT week_commencing,
 FROM radio1_sandbox.vb_sounds_int_users_listening;
 
 --2. Create the table with the summary data and fill it
---2.a Create table
+--2.a Create table -- This is to be inserted into weekly and NOT dropped
+DROP TABLE IF EXISTS radio1_sandbox.vb_sounds_int_KPI;
 CREATE TABLE radio1_sandbox.vb_sounds_int_KPI (
     week_commencing  date,
     country          varchar(400),
@@ -29,9 +32,12 @@ CREATE TABLE radio1_sandbox.vb_sounds_int_KPI (
     signed_in_status varchar(40),
     num_visitors     integer,
     stream_playing_time BIGINT
-);
+);*/
 
 -- 2b. Insert normal data will all the splits
+-- Country Y
+-- App type Y
+-- signed in Y
 INSERT INTO radio1_sandbox.vb_sounds_int_KPI
 SELECT week_commencing,
        country,
@@ -45,6 +51,9 @@ FROM radio1_sandbox.vb_sounds_int_KPI_temp
 GROUP BY 1,2,3,4,5,6
 ;
 -- 2c. Insert with no country split
+-- Country N
+-- App type Y
+-- signed in Y
 INSERT INTO radio1_sandbox.vb_sounds_int_KPI
 SELECT week_commencing,
        CAST('All International' as varchar) AS country,
@@ -59,6 +68,9 @@ GROUP BY 1,2,3,4,5,6
 ;
 
 -- 2d. Insert with no country split AND no app type split
+-- Country N
+-- App type N
+-- signed in Y
 INSERT INTO radio1_sandbox.vb_sounds_int_KPI
 SELECT week_commencing,
        CAST('All International' as varchar) AS country,
@@ -73,6 +85,9 @@ GROUP BY 1,2,3,4,5,6
 ;
 
 -- 2e. Insert with no country split AND no app type split AND no signed in status split
+-- Country N
+-- App type N
+-- signed in N
 INSERT INTO radio1_sandbox.vb_sounds_int_KPI
 SELECT week_commencing,
        CAST('All International' as varchar) AS country,
@@ -86,7 +101,27 @@ FROM radio1_sandbox.vb_sounds_int_KPI_temp
 GROUP BY 1,2,3,4,5,6
 ;
 
--- 2f. Insert with no app type split AND no signed in status split
+-- 2f. Insert with no country split AND no app type split AND no signed in status split
+-- Country N
+-- App type Y
+-- signed in N
+INSERT INTO radio1_sandbox.vb_sounds_int_KPI
+SELECT week_commencing,
+       CAST('All International' as varchar) AS country,
+       age_range,
+       app_type,
+       gender,
+       CAST('all' as varchar) AS signed_in_status,
+       count(distinct audience_id) AS num_visitors,
+       sum(playback_time_3s) AS stream_playing_time
+FROM radio1_sandbox.vb_sounds_int_KPI_temp
+GROUP BY 1,2,3,4,5,6
+;
+
+-- 2g. Insert with no app type split AND no signed in status split
+-- Country Y
+-- App type N
+-- signed in N
 INSERT INTO radio1_sandbox.vb_sounds_int_KPI
 SELECT week_commencing,
        country,
@@ -100,7 +135,10 @@ FROM radio1_sandbox.vb_sounds_int_KPI_temp
 GROUP BY 1,2,3,4,5,6
 ;
 
--- 2g. Insert with no signed in status split
+-- 2h. Insert with no signed in status split
+-- Country Y
+-- App type Y
+-- signed in N
 INSERT INTO radio1_sandbox.vb_sounds_int_KPI
 SELECT week_commencing,
        country,
@@ -113,7 +151,10 @@ SELECT week_commencing,
 FROM radio1_sandbox.vb_sounds_int_KPI_temp
 GROUP BY 1,2,3,4,5,6
 ;
--- 2h. Insert with no app type split
+-- 2i. Insert with no app type split
+-- Country Y
+-- App type N
+-- signed in Y
 INSERT INTO radio1_sandbox.vb_sounds_int_KPI
 SELECT week_commencing,
        country,
@@ -127,5 +168,7 @@ FROM radio1_sandbox.vb_sounds_int_KPI_temp
 GROUP BY 1,2,3,4,5,6
 ;
 
-SELECT DISTINCT app_type, sum(num_visitors) FROM radio1_sandbox.vb_sounds_int_KPI GROUP BY 1;
-SELECT DISTINCT signed_in_status, count(*) FROM radio1_sandbox.vb_sounds_int_KPI GROUP BY 1;
+SELECT DISTINCT week_commencing, signed_in_status, app_type, sum(num_visitors) as num_visitors FROM radio1_sandbox.vb_sounds_int_KPI
+WHERE week_commencing  = '2020-08-17' AND country = 'All International' --AND app_type = 'all' --and signed_in_status = 'all'
+GROUP BY 1,2,3;
+
