a56186651dbe9e8ec2f6951de13f45c08debb2dc
Vicky Banks <victoria.banks052@gmail.com>
2020-09-07 14:11:31 +0100

    Create 4_top_content.sql

---
+++ 4_top_content.sql
@@ -0,0 +1,11 @@
+/*
+ This scripts find the top content viewed per week
+ This will need
+ - number of plays,
+ - number of accounts,
+ - network/podcast/music mix
+ - platform,
+ - live/od
+ - user frequency
+ - age group
+ */
