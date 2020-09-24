4ceb61459084e2759438ce6c18248b3c9f788ab8
Vicky Banks <victoria.banks052@gmail.com>
2020-09-24 10:56:19 +0100

    Added a section to get listner numbers and removed the gender field

--- 2_get_KPI_data.sql
+++ 2_get_KPI_data.sql
@@ -14,52 +14,100 @@ SELECT week_commencing,
        country,
        signed_in_status,
        age_range,
-       gender,
        app_type,
        app_name,
-       CASE WHEN playback_time_total < 3  OR playback_time_total ISNULL THEN 0 ELSE playback_time_total END as playback_time_3s
+       CASE
+           WHEN playback_time_total < 3 OR playback_time_total ISNULL THEN 0
+           ELSE playback_time_total END as playback_time_3s
 FROM radio1_sandbox.vb_sounds_int_users_listening;
 
---2. Create the table with the summary data and fill it
---2.a Create table -- This is to be inserted into weekly and NOT dropped
+-- 2. Count listeners
+DROP TABLE IF EXISTS radio1_sandbox.vb_sounds_int_KPI_listeners;
+CREATE TABLE radio1_sandbox.vb_sounds_int_KPI_listeners AS
+SELECT week_commencing,
+       country,
+       age_range,
+       app_type,
+       signed_in_status,
+       count(distinct audience_id) as num_listeners
+      FROM radio1_sandbox.vb_sounds_int_users_listening
+    WHERE playback_time_total < 3
+GROUP BY 1,2,3,4,5;
+
+INSERT INTO radio1_sandbox.vb_sounds_int_KPI_listeners
+SELECT week_commencing,
+       country,
+       age_range,
+       CAST('all' AS varchar) AS app_type,
+       signed_in_status,
+       count(distinct audience_id) as num_listeners
+      FROM radio1_sandbox.vb_sounds_int_users_listening
+    WHERE playback_time_total < 3
+GROUP BY 1,2,3,4,5;
+
+
+--3. Create the table with the summary data and fill it
+--3.a Create table -- This is to be inserted into weekly and NOT dropped
 /*DROP TABLE IF EXISTS radio1_sandbox.vb_sounds_int_KPI;
-CREATE TABLE radio1_sandbox.vb_sounds_int_KPI (
-    week_commencing  date,
-    country          varchar(400),
-    age_range        varchar(40),
-    app_type         varchar(40),
-    gender           varchar(40),
-    signed_in_status varchar(40),
-    num_visitors     integer,
-    stream_playing_time BIGINT
+CREATE TABLE radio1_sandbox.vb_sounds_int_KPI
+(
+    week_commencing     date,
+    country             varchar(400),
+    age_range           varchar(40),
+    app_type            varchar(40),
+    signed_in_status    varchar(40),
+    num_visitors        integer,
+    stream_playing_time BIGINT,
+    num_listeners       bigint
 );*/
 
 -- 2.a Insert data
 INSERT INTO radio1_sandbox.vb_sounds_int_KPI
-SELECT week_commencing,
-       country,
-       age_range,
-       app_type,
-       gender,
-       signed_in_status,
-       count(distinct audience_id) AS num_visitors,
-       sum(playback_time_3s) AS stream_playing_time
-FROM radio1_sandbox.vb_sounds_int_KPI_temp
-GROUP BY 1,2,3,4,5,6
+with visitors as (
+    SELECT week_commencing,
+           country,
+           age_range,
+           app_type,
+           signed_in_status,
+           count(distinct audience_id) AS num_visitors,
+           sum(playback_time_3s)       AS stream_playing_time
+    FROM radio1_sandbox.vb_sounds_int_KPI_temp
+    GROUP BY 1, 2, 3, 4, 5
+)
+SELECT a.*, b.num_listeners
+FROM visitors a
+         LEFT JOIN radio1_sandbox.vb_sounds_int_KPI_listeners b ON
+        a.week_commencing = b.week_commencing AND
+        a.country = b.country AND
+        a.age_range = b.age_range AND
+        a.app_type = b.app_type AND
+        a.signed_in_status = b.signed_in_status
 ;
+
+
 -- Insert data but dedup across app type
 INSERT INTO radio1_sandbox.vb_sounds_int_KPI
-SELECT week_commencing,
-       country,
-       age_range,
-       CAST('all' as varchar) AS app_type,
-       gender,
-       signed_in_status,
-       count(distinct audience_id) AS num_visitors,
-       sum(playback_time_3s) AS stream_playing_time
-FROM radio1_sandbox.vb_sounds_int_KPI_temp
-GROUP BY 1,2,3,4,5,6
+with visitors as (
+    SELECT week_commencing,
+           country,
+           age_range,
+           CAST('all' as varchar)      AS app_type,
+           signed_in_status,
+           count(distinct audience_id) AS num_visitors,
+           sum(playback_time_3s)       AS stream_playing_time
+    FROM radio1_sandbox.vb_sounds_int_KPI_temp
+    GROUP BY 1, 2, 3, 4, 5
+)
+SELECT a.*, b.num_listeners
+FROM visitors a
+         LEFT JOIN radio1_sandbox.vb_sounds_int_KPI_listeners b ON
+        a.week_commencing = b.week_commencing AND
+        a.country = b.country AND
+        a.age_range = b.age_range AND
+        a.app_type = b.app_type AND
+        a.signed_in_status = b.signed_in_status
 ;
+
 
 -- Stakeholder friendly language
 UPDATE radio1_sandbox.vb_sounds_int_KPI
@@ -72,8 +120,10 @@ SET app_type = (CASE
 ;
 
 SELECT * FROM radio1_sandbox.vb_sounds_int_KPI LIMIT 10;
-SELECT DISTINCT week_commencing FROM radio1_sandbox.vb_sounds_int_KPI ORDER BY 1;
+
 GRANT ALL ON radio1_sandbox.vb_sounds_int_KPI to helen_jones;
+
 
 ---- Drop tables
 DROP TABLE IF EXISTS radio1_sandbox.vb_sounds_int_KPI_temp;
+DROP TABLE IF EXISTS radio1_sandbox.vb_sounds_int_KPI_listeners;
